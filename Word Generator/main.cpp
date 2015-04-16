eeed4b5f5602d8bbca462366d8ff28d557494f79
Zoltán Majoros <zoltan@arcanelab.com>
2015-04-16 16:04:10 +0200

    Initial Commit

---
+++ Word Generator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Word Generator
+//
+//  Created by Zoltán Majoros on 16/Apr/15.
+//  Copyright (c) 2015 Zoltán Majoros. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
